a3764cc84bb81e3c080294d37540a4da95ff7df4
Santhosh <nannisantosh@gmail.com>
2024-02-07 04:55:05 +0530

    remove duplicate courses

--- server/controllers/course.js
+++ server/controllers/course.js
@@ -3,10 +3,11 @@ import connection from "../config/db.js";
 export const listCourses = async (req, res) => {
   try {
     const { name = "", country = "", level = "" } = req.query;
-    const query = `SELECT * FROM Country JOIN University ON Country.country_id = University.country_id 
+    const query = `SELECT DISTINCT * FROM Country JOIN University ON Country.country_id = University.country_id 
                              JOIN Course ON Course.university_id = University.university_id 
                              JOIN EducationLevel ON EducationLevel.level_id = Course.level_id
-                             JOIN Scholarship ON Scholarship.university_id = University.university_id
+                             JOIN Scholarship ON Scholarship.university_id = University.university_id AND 
+                             Scholarship.level_id = EducationLevel.level_id
                              WHERE Country.country_name LIKE ? AND 
                              EducationLevel.level_name LIKE ? AND 
                              (Course.course_name LIKE ? OR University.university_name LIKE ?)`;
